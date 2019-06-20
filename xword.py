d39574a215146603d7c3544368c2c9c80a9f5dca
KashF <Kashfarhadi@gmail.com>
2019-06-20 16:12:32 -0400

    finished solver

--- xword.py
+++ xword.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "Kashfar"
 
 # YOUR HELPER FUNCTION GOES HERE
 
@@ -13,9 +13,18 @@ def main():
 
     test_word = raw_input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+    regex_word = test_word.replace(' ', '\\w')
+    result = [
+        word for word in words if len(test_word) == len(word) and re.match(regex_word, word)
+    ]
+    # for word in words:
+    #     if len(test_word) == len(word):
+    #         regex_word = test_word.replace(' ', '\\w')
 
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    #         match_object = re.match(regex_word, word)
+    #         if match_object:
+    #             result.append(word)
+    print(result)
 
 
 if __name__ == '__main__':
